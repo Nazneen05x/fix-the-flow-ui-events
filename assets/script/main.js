44f16fdbd7110fe5473c44b9695448a5084041fd
Nazneen05x <112861261+Nazneen05x@users.noreply.github.com>
2022-12-13 16:56:30 +0100

    yes

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,23 +1,37 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
 let flow = document.querySelector('a:nth-of-type(8)')
+let the = document.querySelector('a:nth-of-type(7)')
 let sprint5 = document.querySelector('a:nth-of-type(5)')
+let interface = document.querySelector('a:nth-of-type(10)')
+
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
-flow.addEventListener('click', slideclick)
-sprint5.addEventListener('click', sprint5click)
+flow.addEventListener('click', colorclick)
+the.addEventListener('click', grootclick)
+sprint5.addEventListener('click', slideclick)
+interface.addEventListener('click', slideeclick)
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
-function slideclick () {
-  flow.classList.toggle('slide')
+function colorclick () {
+  flow.classList.toggle('color')
 }
 
-function sprint5click () {
-  flow.classList.toggle('slide_1')
+function grootclick () {
+   the.classList.toggle('groot')
 }
+function slideclick(){
+  sprint5.classList.toggle('slide')
+}
+
+function slideeclick() {
+  interface.classList.toggle('slidee')
+}
+
+
 
 
 
