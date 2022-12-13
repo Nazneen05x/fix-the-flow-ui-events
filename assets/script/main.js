e71d7fdff6fe0121ba854f422de4a4883518ac7e
Nazneen05x <112861261+Nazneen05x@users.noreply.github.com>
2022-12-13 22:24:26 +0100

    yeah

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,6 +3,8 @@ let flow = document.querySelector('a:nth-of-type(8)')
 let the = document.querySelector('a:nth-of-type(7)')
 let sprint5 = document.querySelector('a:nth-of-type(5)')
 let interface = document.querySelector('a:nth-of-type(10)')
+let and = document.querySelector('a:nth-of-type(3)')
+let design = document.querySelector('a:nth-of-type(2)')
 
 
 interaction.addEventListener('click', jumpHandler)
@@ -11,6 +13,8 @@ flow.addEventListener('click', colorclick)
 the.addEventListener('click', grootclick)
 sprint5.addEventListener('click', slideclick)
 interface.addEventListener('click', slideeclick)
+and.addEventListener('click', rondclick)
+design.addEventListener('click', flipclick)
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
@@ -31,8 +35,13 @@ function slideeclick() {
   interface.classList.toggle('slidee')
 }
 
+function rondclick () {
+  and.classList.toggle('rond')
+}
 
-
+function flipclick () {
+  design.classList.toggle('flip')
+}
 
 
 
