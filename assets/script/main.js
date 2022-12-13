1df7ccb793960f3390c802486ef6ec74f0a4d54f
Nazneen05x <112861261+Nazneen05x@users.noreply.github.com>
2022-12-13 12:38:50 +0100

    ugh

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,25 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
+let flow = document.querySelector('a:nth-of-type(8)')
+let sprint5 = document.querySelector('a:nth-of-type(5)')
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
+flow.addEventListener('click', slideclick)
+sprint5.addEventListener('click', sprint5click)
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+function slideclick () {
+  flow.classList.toggle('slide')
+}
+
+function sprint5click () {
+  flow.classList.toggle('slide_1')
+}
+
+
+
+
+
